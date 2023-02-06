e84bce447cad40aa91fe36dfeb5d25f2588bdcca
Senya <senya.korotkov@gmail.com>
2023-02-06 19:14:08 +0300

    refactoring to flake8

--- lyceum_django_education_project/settings.py
+++ lyceum_django_education_project/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)-6e' \
-             'gfo$t17d+#j()=c2cng6#f1xpthg9ab8#rdv#m$&!5c0mw'
+SECRET_KEY = 'NOT A SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
