5ecc36ae29e8db51da6ad7445533a0b9e95cf4e6
Senya <senya.korotkov@gmail.com>
2023-02-13 12:21:37 +0300

    refactoring to flake8

--- catalog/tests.py
+++ catalog/tests.py
@@ -8,38 +8,32 @@ class StaticURLTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_catalog_number_endpoint(self):
-        response = Client().get(f'/catalog/0/')
+        response = Client().get('/catalog/0/')
         self.assertEqual(response.status_code, 200)
 
-    @parameterized.parameterized.expand([(-1,),
-                                         ('adf',),
-                                         (1.8,)])
+    @parameterized.parameterized.expand([(-1,), ('adf',), (1.8,)])
     def test_catalog_number_endpoint_negative(self, number):
         response = Client().get(f'/catalog/{number}/')
         self.assertEqual(response.status_code, 404)
 
     def test_catalog_converter_endpoint(self):
-        response = Client().get(f'/catalog/converter/0/')
+        response = Client().get('/catalog/converter/0/')
         self.assertEqual(response.status_code, 200)
 
-    @parameterized.parameterized.expand([(-1,),
-                                         ('adf',),
-                                         (1.8,),
-                                         ('00',),
-                                         ('025',)])
+    @parameterized.parameterized.expand(
+        [(-1,), ('adf',), (1.8,), ('00',), ('025',)]
+    )
     def test_catalog_converter_endpoint_negative(self, number):
         response = Client().get(f'/catalog/converter/{number}/')
         self.assertEqual(response.status_code, 404)
 
     def test_catalog_re_endpoint(self):
-        response = Client().get(f'/catalog/re/0/')
+        response = Client().get('/catalog/re/0/')
         self.assertEqual(response.status_code, 200)
 
-    @parameterized.parameterized.expand([(-1,),
-                                         ('adf',),
-                                         (1.8,),
-                                         ('00',),
-                                         ('025',)])
+    @parameterized.parameterized.expand(
+        [(-1,), ('adf',), (1.8,), ('00',), ('025',)]
+    )
     def test_catalog_re_endpoint_negative(self, number):
         response = Client().get(f'/catalog/re/{number}/')
         self.assertEqual(response.status_code, 404)
