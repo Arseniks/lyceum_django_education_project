5664e8d711216c227ab53547caeee407853e3724
Arseniks <senya.korotkov@gmail.com>
2023-02-27 16:53:05 +0300

    refactoring requirements

--- lyceum_django_education_project/settings.py
+++ lyceum_django_education_project/settings.py
@@ -138,3 +138,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
+
+GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
+GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
