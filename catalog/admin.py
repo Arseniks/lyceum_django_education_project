9b239f3aedbac7d9d42c35e2562e4ee51349659e
Arseniks <senya.korotkov@gmail.com>
2023-02-27 17:43:52 +0300

    refactoring linting

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
-from django.contrib import admin
 from django import forms
+from django.contrib import admin
 
 import catalog.models
 
