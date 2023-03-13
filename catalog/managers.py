5f411f14fcf0a8fcc6f8f041e4cfef1bbd710e88
Arseniks <senya.korotkov@gmail.com>
2023-03-14 00:40:52 +0300

    refactoring hard coding

--- catalog/managers.py
+++ catalog/managers.py
@@ -4,24 +4,36 @@ from django.db.models import Prefetch
 
 class ItemManager(models.Manager):
     def published(self):
+        from catalog.models import Item
+        from catalog.models import MainImage
         from catalog.models import Tag
 
         return (
             self.get_queryset()
-            .select_related('category', 'mainimage')
+            .select_related(
+                Item.category.field.name, Item.mainimage.related.name
+            )
             .filter(is_published=True, category__is_published=True)
             .prefetch_related(
-                Prefetch('tags', queryset=Tag.objects.published())
+                Prefetch(
+                    Item.tags.field.name, queryset=Tag.objects.published()
+                )
             )
             .only(
-                'name',
-                'text',
-                'category__name',
-                'mainimage__image',
+                Item.name.field.name,
+                Item.text.field.name,
+                f'{Item.category.field.name}__{Item.name.field.name}',
+                f'{Item.mainimage.related.name}__{MainImage.image.field.name}',
             )
         )
 
 
 class TagManager(models.Manager):
     def published(self):
-        return self.get_queryset().filter(is_published=True).only('name')
+        from catalog.models import Tag
+
+        return (
+            self.get_queryset()
+            .filter(is_published=True)
+            .only(Tag.name.field.name)
+        )
