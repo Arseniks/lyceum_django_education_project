d6fe2fde958434866143fdc0a65e8ee76279e8f1
Arseniks <senya.korotkov@gmail.com>
2023-03-24 16:17:19 +0300

    updating sh script

--- project_setup.sh
+++ project_setup.sh
@@ -2,7 +2,6 @@
 
 set -ex
 
-cd lyceum_django_education_project
 echo "Creating Virtual Environment"
 python -m venv venv
 echo "Activating Virtual Environment"
@@ -10,12 +9,11 @@ source venv/bin/activate
 echo "Switching git branch"
 git checkout $GIT_BRANCH
 echo "Creating .env, don't forget to change values"
-touch .env
 cp .env.template .env
 echo "Installing dependencies"
-pip install -r requirements.txt
-pip install -r requirements_dev.txt
-pip install -r requirements_test.txt
+pip install -r requirements/requirements.txt
+pip install -r requirements/requirements_dev.txt
+pip install -r requirements/requirements_test.txt
 echo "Installing migrations"
 python manage.py migrate
 echo "Filling the database from a fixture"
