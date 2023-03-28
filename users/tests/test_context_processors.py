cfe2bc15f8293fb8c17920d59a2038e0a199c12d
JeeEssEm <greendiesel228@gmail.com>
2023-03-28 21:45:12 +0300

    black cheeeeck

--- users/tests/test_context_processors.py
+++ users/tests/test_context_processors.py
@@ -8,7 +8,6 @@ from users import models
 
 
 class ContextTest(django.test.TestCase):
-
     @classmethod
     def setUpTestData(cls):
         cls.user = models.User.objects.create_user(
@@ -16,32 +15,44 @@ class ContextTest(django.test.TestCase):
             password='tang0_mang0',
             email='nota@yahoo.com',
         )
-        cls.profile = models.Profile.objects.create(
-            user_id=cls.user.id
-        )
+        cls.profile = models.Profile.objects.create(user_id=cls.user.id)
 
-    @parameterized.parameterized.expand([
-        ('29.04.2023', '29.04.2023', 1,),
-        ('29.04.2023', '28.04.2023', 0,),
-        ('29.04.2002', '29.04.2023', 1,),
-        ('29.03.2002', '29.04.2023', 0,),
-        ('30.03.2002', '29.04.2023', 0,),
-    ])
+    @parameterized.parameterized.expand(
+        [
+            (
+                '29.04.2023',
+                '29.04.2023',
+                1,
+            ),
+            (
+                '29.04.2023',
+                '28.04.2023',
+                0,
+            ),
+            (
+                '29.04.2002',
+                '29.04.2023',
+                1,
+            ),
+            (
+                '29.03.2002',
+                '29.04.2023',
+                0,
+            ),
+            (
+                '30.03.2002',
+                '29.04.2023',
+                0,
+            ),
+        ]
+    )
     def test_birthday_user(self, cookie_date, user_birth, result):
         date = user_birth
-        self.profile.birthday = datetime.datetime.strptime(
-            date, '%d.%m.%Y'
-        )
+        self.profile.birthday = datetime.datetime.strptime(date, '%d.%m.%Y')
         self.profile.save()
 
-        self.client.cookies.load({
-            'django_date': cookie_date
-        })
+        self.client.cookies.load({'django_date': cookie_date})
         response = self.client.get(
-            django.shortcuts.reverse('homepage:home'),
-            follow=True
+            django.shortcuts.reverse('homepage:home'), follow=True
         )
-        self.assertEqual(
-            result,
-            response.context['births'].count()
-        )
+        self.assertEqual(result, response.context['births'].count())
