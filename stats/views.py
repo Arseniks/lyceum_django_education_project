ae8b53c0d2bda7f73c3fef26b25927d668f6cae6
Starostina Helen <starostina.lena05@gmail.com>
2023-03-28 21:38:23 +0400

    tests for Stats app: short user's statistic. Bug with not changing time_created field when the mark is changing was fixed

--- stats/views.py
+++ stats/views.py
@@ -8,14 +8,20 @@ class ShortUser(View):
     template_name = 'stats/short_user_stat.html'
 
     def get(self, request):
-        marks = [
-            (i.mark, i.date_created, i.get_mark_display(), i.item)
-            for i in Mark.objects.filter(user=request.user)
-        ]
-        context = {
-            'best_mark': max(marks, key=lambda el: (el[0], el[1])),
-            'worst_mark': max(marks, key=lambda el: (-el[0], el[1])),
-            'count': len(marks),
-            'middle_value': sum([i[0] for i in marks]) / len(marks),
-        }
+        if request.user.id:
+            marks = [
+                (i.mark, i.date_created, i.get_mark_display(), i.item)
+                for i in Mark.objects.filter(user=request.user)
+            ]
+            if marks:
+                context = {
+                    'best_mark': max(marks, key=lambda el: (el[0], el[1])),
+                    'worst_mark': max(marks, key=lambda el: (-el[0], el[1])),
+                    'count': len(marks),
+                    'middle_value': sum([i[0] for i in marks]) / len(marks),
+                }
+            else:
+                context = {'message': 'Вы пока не поставили ни одной оценки'}
+        else:
+            context = {'message': 'Войдите, чтобы просмотреть статистику'}
         return render(request, self.template_name, context)
