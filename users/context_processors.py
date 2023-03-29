d635bca3a18e1d4483b6ff1f3e3a892f2b9e271b
Arseniks <senya.korotkov@gmail.com>
2023-03-29 22:17:16 +0300

    refactoring context_processors

--- users/context_processors.py
+++ users/context_processors.py
@@ -8,27 +8,30 @@ import users.models
 
 
 def birthday_people(request):
-    births = (
-        users.models.Profile.objects.activated()
-        .only(
-            f'{users.models.Profile.user.field.name}__'
-            f'{users.models.User.email.field.name}',
-            f'{users.models.Profile.user.field.name}__'
-            f'{users.models.User.first_name.field.name}',
-            users.models.Profile.birthday.field.name,
-        )
-        .filter(
-            birthday__day__range=[
-                (django.utils.timezone.now() - datetime.timedelta(hours=26)).day,
-                (django.utils.timezone.now() + datetime.timedelta(hours=26)).day,
-            ],
-            birthday__month__range=[
-                (django.utils.timezone.now() - datetime.timedelta(hours=26)).month,
-                (django.utils.timezone.now() + datetime.timedelta(hours=26)).month,
-            ],
+    active_user_with_births = users.models.Profile.objects.activated().filter(birthday__isnull=False)
+    births = []
+    if active_user_with_births:
+        births = (
+            users.models.Profile.objects.activated()
+            .only(
+                f'{users.models.Profile.user.field.name}__'
+                f'{users.models.User.email.field.name}',
+                f'{users.models.Profile.user.field.name}__'
+                f'{users.models.User.first_name.field.name}',
+                users.models.Profile.birthday.field.name,
+            )
+            .filter(
+                birthday__day__range=[
+                    (django.utils.timezone.now() - datetime.timedelta(hours=26)).day,
+                    (django.utils.timezone.now() + datetime.timedelta(hours=26)).day,
+                ],
+                birthday__month__range=[
+                    (django.utils.timezone.now() - datetime.timedelta(hours=26)).month,
+                    (django.utils.timezone.now() + datetime.timedelta(hours=26)).month,
+                ],
 
+            )
         )
-    )
     return {
         'births': births,
     }
