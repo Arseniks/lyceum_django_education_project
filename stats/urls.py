165f30f08c048bf41e6ebea624d771a735a3493a
JeeEssEm <greendiesel228@gmail.com>
2023-03-29 19:10:46 +0300

    created view for item statistic which gets by typing id in browser. All information about item stat gets by SQL queries in get_context_data method of ItemStats class which inherits from DetailView

--- stats/urls.py
+++ stats/urls.py
@@ -6,4 +6,5 @@ app_name = 'stats'
 
 urlpatterns = [
     path('user_stat_short/', views.ShortUser.as_view(), name='short_user'),
+    path('item_stat/<int:pk>/', views.ItemStats.as_view(), name='item_stat'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,6 +1,12 @@
+from django.db.models import Avg
+from django.db.models import Count
+from django.db.models import Max
+from django.db.models import Min
 from django.shortcuts import render
+from django.views.generic import DetailView
 from django.views.generic import View
 
+from catalog.models import Item
 from rating.models import Mark
 
 
@@ -25,3 +31,45 @@ class ShortUser(View):
         else:
             context = {'message': 'Войдите, чтобы просмотреть статистику'}
         return render(request, self.template_name, context)
+
+
+class ItemStats(DetailView):
+    template_name = 'stats/item_stat.html'
+    model = Item
+
+    def get_context_data(self, **kwargs):
+        context = super(ItemStats, self).get_context_data(**kwargs)
+        item = context.get('item')
+        item_marks = Mark.objects.filter(item=item)
+
+        if item_marks.exists():
+            stat_info = item_marks.aggregate(
+                Min(Mark.mark.field.name),
+                Max(Mark.mark.field.name),
+                Avg(Mark.mark.field.name),
+                Count(Mark.mark.field.name),
+            )
+            avg_rating = stat_info[f'{Mark.mark.field.name}__avg']
+            mark_count = stat_info[f'{Mark.mark.field.name}__count']
+
+            max_mark = stat_info[f'{Mark.mark.field.name}__max']
+            min_mark = stat_info[f'{Mark.mark.field.name}__min']
+
+            max_user = item_marks.select_related(
+                f'{Mark.user.field.name}'
+            ).get(mark=max_mark)
+
+            min_user = item_marks.select_related(
+                f'{Mark.user.field.name}'
+            ).get(mark=min_mark)
+
+            context.update(
+                {
+                    'avg_rating': avg_rating,
+                    'mark_count': mark_count,
+                    'max_user': max_user,
+                    'min_user': min_user,
+                }
+            )
+
+        return context
