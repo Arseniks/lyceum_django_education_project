2b17e8907393c2f6676faf1d2a364a7a2eeb3fa9
Arseniks <senya.korotkov@gmail.com>
2023-03-29 22:57:30 +0300

    refactoring to linting

--- stats/urls.py
+++ stats/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
         'user_rated_items_list/<int:pk>/',
         views.UserRatedItemsList.as_view(),
         name='user_rated_items_list',
+    ),
     path('item_stat/<int:pk>/', views.ItemStats.as_view(), name='item_stat'),
     path(
         'user_stat_short/<int:pk>',
